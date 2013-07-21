f291040584c102de6190b7613ba599efc878ba1f
Yaron <justame@gmail.com>
2013-07-21 10:45:32 +0300

    working demo

--- app/js/index.js
+++ app/js/index.js
@@ -7,28 +7,38 @@ hpApp.controller('youtube-playlist', function($scope, $resource){
   var videoResource = $resource('youtube/videos/',{}); 
   $scope.videoList = videoResource.query({query: 'dbz'}).$then(function(response){
     $scope.videoList = response.data.data.items;
-  })
+  });
 
-  $scope.playlist = [];
+  $scope.playlist = JSON.parse(localStorage.getItem("playlist"));
 
   $scope.selectedVideo = null;
 
+  function _updateLocalStorage(){
+    localStorage.setItem("playlist",JSON.stringify($scope.playlist));
+  }
+  
   $scope.addToPlaylist = function(index){
     var video = $scope.videoList[index];
     if($scope.playlist.indexOf(video) == -1){
       $scope.playlist.push(video);
+      _updateLocalStorage();
     };
   };
 
   $scope.removeFromPlaylist = function(index){
     $scope.playlist.splice(index,1);
+    _updateLocalStorage();
   };
 
   $scope.selectVideo = function(index){
     $scope.selectedVideo = $scope.videoList[index];
   };
-
-
+  
+  $scope.search = function(){
+    videoResource.query({query: $scope.searchStr}).$then(function(response){
+      $scope.videoList = response.data.data.items;
+    });
+  };
 
 });
 
